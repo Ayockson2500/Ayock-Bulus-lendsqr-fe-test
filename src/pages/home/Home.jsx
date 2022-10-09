3555f8aaf458a5d737b2d59ac30a44b8280512d8
ayockson2500 <ayockson2500@gmail.com>
2022-10-09 18:31:13 +0100

    make api call on home screen

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,36 +1,56 @@
-import Sidebar from '../../components/sidebar/Sidebar'
-import Navbar from '../../components/navbar/Navbar'
-import './home.scss'
-import Widget from '../../components/widget/Widget'
-import Table from '../../components/userTable/Table'
-import { FaUserPlus } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import "./home.scss";
+import Widget from "../../components/widget/Widget";
+import Table from "../../components/userTable/Table";
+import { FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { getAllUsers } from "../../api/services/user";
+import useLocalStorage from "../../utils/useLocalStorage";
 
 export default function Home() {
-  return (
-    <div className='home'>
-        <Sidebar />
+  const [users, setUsers] = useState([]);
+  const { getItem, setItem } = useLocalStorage("users");
 
-         <div className="homeContainer">
-          <Navbar />
-          <div className='userDash'>
-            <Link className='link' to='/singleUser'>
-           <span>Users</span> 
-           </Link>
-           
-           <Link to='/addUser'>
-           <span className='addIcon'><FaUserPlus /></span> 
-           </Link>
-          </div>
-          <div className='widgets'>
-            <Widget type='users' />
-            <Widget type='active_users' />
-            <Widget type='users_with_loans' />
-            <Widget type='users_with_savings' />
-          </div>
-         
-            <Table />
+  useEffect(() => {
+    const usersData = getItem();
+    if (usersData) {
+      setUsers(usersData);
+    } else {
+      getAllUsers().then((res) => {
+        setUsers(res.data);
+        setItem(res.data);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="home">
+      <Sidebar />
+
+      <div className="homeContainer">
+        <Navbar />
+        <div className="userDash">
+          <Link className="link" to="/singleUser">
+            <span>Users</span>
+          </Link>
+
+          <Link to="/addUser">
+            <span className="addIcon">
+              <FaUserPlus />
+            </span>
+          </Link>
         </div>
+        <div className="widgets">
+          <Widget type="users" />
+          <Widget type="active_users" />
+          <Widget type="users_with_loans" />
+          <Widget type="users_with_savings" />
+        </div>
+
+        <Table tableData={users} />
+      </div>
     </div>
-  )
+  );
 }
