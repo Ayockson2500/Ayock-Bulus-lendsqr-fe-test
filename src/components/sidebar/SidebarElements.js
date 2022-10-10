8a0f0fd72fca2f7a9c8c60b243310e4f08d0c709
ayockson2500 <ayockson2500@gmail.com>
2022-10-10 14:05:53 +0100

    created a seperate file for side bar menu items

---
+++ src/components/sidebar/SidebarElements.js
@@ -0,0 +1,115 @@
+import gurantor from "../../img/sidebar/gurantor.png";
+import loans from "../../img/sidebar/loans.png";
+import handshake from "../../img/sidebar/handshake.png";
+import savings from "../../img/sidebar/savings.png";
+import loanrequest from "../../img/sidebar/loanrequest.png";
+import whitelist from "../../img/sidebar/whitelist.png";
+import karma from "../../img/sidebar/karma.png";
+import briefcaseOrg from "../../img/sidebar/briefcaseOrg.png";
+import loanproducts from "../../img/sidebar/loanproducts.png";
+import savingproduct from "../../img/sidebar/savingproduct.png"; 
+import feeandcharges from "../../img/sidebar/feeandcharges.png";
+import transactions from "../../img/sidebar/transactions.png";
+import services from "../../img/sidebar/services.png";
+import servicesacount from "../../img/sidebar/servicesacount.png";
+import settlement from "../../img/sidebar/settlement.png";
+import reports from "../../img/sidebar/reports.png";
+import prefrence from "../../img/sidebar/prefrence.png";
+import feeandpricing from "../../img/sidebar/feeandpricing.png";
+import auditlogs from "../../img/sidebar/auditlogs.png";
+import systemmesssage from "../../img/sidebar/systemmesssage.png";
+import logout from "../../img/sidebar/logout.png";
+
+
+export const CustomerElements = [
+    {
+        icon: gurantor,
+        title: "Guarantors"
+    },
+    {
+        icon: loans,
+        title: "Loans" 
+    },
+    {
+        icon: handshake,
+        title: "Decision Models"
+    },
+    {
+        icon: savings,
+        title: "Savings"
+    },
+    {
+        icon: loanrequest,
+        title: "Loan Requests"
+    },
+    {
+        icon: whitelist,
+        title: "Whitelist"
+    },
+    {
+        icon: karma,
+        title: "Karma"
+    }
+]
+
+export const BusinessElements = [
+    {
+        icon: briefcaseOrg,
+        title: "Organization"
+    },
+    {
+        icon: loanproducts,
+        title: "Loan Products" 
+    },
+    {
+        icon: savingproduct,
+        title: "Savings Products"
+    },
+    {
+        icon: feeandcharges,
+        title: "Fees and Charges"
+    },
+    {
+        icon: transactions,
+        title: "Transactions"
+    },
+    {
+        icon: services,
+        title: "Services"
+    },
+    {
+        icon: servicesacount,
+        title: "Service Account"
+    },
+    {
+        icon: settlement,
+        title: "Settlements"
+    },
+    {
+        icon: reports,
+        title: "Reports"
+    },
+]
+
+export const SettingsElements = [
+    {
+        icon: prefrence,
+        title: "Preferences"
+    },
+    {
+        icon: feeandpricing,
+        title: "Fees and Pricing"
+    },
+    {
+        icon: auditlogs,
+        title: "Audit Logs"
+    },
+    {
+        icon: systemmesssage,
+        title: "Systems Messages"
+    },
+    {
+        logOutIcon: logout,
+        logOutTitle: "Logout"
+    }
+]
